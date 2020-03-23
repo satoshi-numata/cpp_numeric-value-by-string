3296d33c3387a09dd2156da60caf4a8b5afacc8f
Satoshi Numata <numata@osakac.ac.jp>
2020-03-23 19:07:04 +0900

    自然対数の底を求める計算をしてみました。

--- FPValueExp/main.cpp
+++ FPValueExp/main.cpp
@@ -21,7 +21,19 @@ int main()
         printf("value1/value2=[%s]\n", FPValue::Div(value1, value2, 20, true).c_str());
         printf("value1%%value2=[%s]\n", (value1 % value2).c_str());*/
 
-        printf("value1^value2=%s\n", FPValue::Pow(value1, value2, 100).c_str());
+        printf("value1^value2=%s\n", FPValue::Pow(value1, value2, 9).c_str());
+
+        // 自然対数の底を求めてみる
+        FPValue e("1");
+        FPValue fact("1");
+        FPValue dfact("2");
+        for (int i = 0; i < 40; i++) {
+            e = e + FPValue::Div(FPValue("1"), fact, 40, true);
+            printf("%d: e=%s\n", i, e.c_str());
+            fact = fact * dfact;
+            dfact = dfact + FPValue("1");
+        }
+        printf("e=%s\n", e.c_str());
 
     } catch (std::exception& e) {
         printf("Error: %s\n", e.what());
