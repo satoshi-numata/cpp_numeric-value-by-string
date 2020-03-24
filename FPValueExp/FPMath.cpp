d1969baa7f732109986d9d4a57c96447cbafc403
Satoshi Numata <numata@osakac.ac.jp>
2020-03-24 09:35:38 +0900

    四則演算以外の複雑な計算について、FPMathクラスを用意して、そこに関数をまとめるようにしました。
    - 整数の階乗を計算する関数を用意しました。

---
+++ FPValueExp/FPMath.cpp
@@ -0,0 +1,9 @@
+//
+//  FPMath.cpp
+//  FPValueExp
+//
+//  Created by numata on 2020/03/24.
+//  Copyright © 2020 Satoshi Numata. All rights reserved.
+//
+
+#include "FPMath.hpp"
---
+++ FPValueExp/FPMath.hpp
@@ -0,0 +1,14 @@
+//
+//  FPMath.hpp
+//  FPValueExp
+//
+//  Created by numata on 2020/03/24.
+//  Copyright © 2020 Satoshi Numata. All rights reserved.
+//
+
+#ifndef FPMath_hpp
+#define FPMath_hpp
+
+#include <stdio.h>
+
+#endif /* FPMath_hpp */
